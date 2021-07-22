402ef11a5b7d9879d324b32bb6162262999b5cd2
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2021-07-22 13:36:00 -0400

    Remove extra dice set
    Game only has one set of dice.  Remove "attack" and "defense" aspects.

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 // PACIFIC BATTLE //
 
 // Global Variables
-const diceBox = document.querySelector('.dicebox')
-const attackBox = document.querySelector('#attack')
-const defenseBox = document.querySelector('#defense')
-const attackDice = []
-const defenseDice = []
+const diceBox = document.querySelector('.diceBox')
+let diceResults = []
 
 // Die roller
-function rollDie(box, dice, rolls) {
-    for (let i=0; i<rolls; i++) {
-        const roll = Math.ceil(Math.random()*6)
-        // console.log(roll)        
-        dice.push(roll)
-        // console.log(dice);
-        const die = document.createElement('div')
-        die.classList.add('die')
-        die.innerHTML = roll
-        box.appendChild(die)
-    }
+function rollDie(rolls) {
+  for (let i = 0; i < rolls; i++) {
+    const roll = Math.ceil(Math.random() * 6)
+    // console.log(roll)
+    diceResults.push(roll)
+    // console.log(dice);
+    const die = document.createElement('div')
+    die.classList.add('die')
+    die.innerHTML = roll
+    diceBox.appendChild(die)
+  }
 }
-rollDie(attackBox, attackDice, 4)
-rollDie(defenseBox, defenseDice, 6)
+rollDie(6)
