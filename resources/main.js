e43af62d9639817742089982cfe9f5bc2c5e2632
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-07-21 22:21:50 -0400

    Working dice generator
    Attack and Defense dice sets

--- resources/main.js
+++ resources/main.js
@@ -1,2 +1,24 @@
 // PACIFIC BATTLE //
 
+// Global Variables
+const diceBox = document.querySelector('.dicebox')
+const attackBox = document.querySelector('#attack')
+const defenseBox = document.querySelector('#defense')
+const attackDice = []
+const defenseDice = []
+
+// Die roller
+function rollDie(box, dice, rolls) {
+    for (let i=0; i<rolls; i++) {
+        const roll = Math.ceil(Math.random()*6)
+        // console.log(roll)        
+        dice.push(roll)
+        // console.log(dice);
+        const die = document.createElement('div')
+        die.classList.add('die')
+        die.innerHTML = roll
+        box.appendChild(die)
+    }
+}
+rollDie(attackBox, attackDice, 4)
+rollDie(defenseBox, defenseDice, 6)
