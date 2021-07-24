999a75941b8ead10e6aea831f95a32cf2f1d9734
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-07-23 20:30:29 -0400

    Resolve ship attack
    Create damage and sunk properties for ships.
    Change dice roll to local variable returned.
    Add attack function with hits tally and damage applied.

--- main.js
+++ main.js
@@ -19,12 +19,16 @@ const alliedFleet = [
     side: 'allied',
     attack: 4,
     defense: 4,
+    damage: 0,
+    sunk: false
   },
   {
     name: 'West Virginia',
     side: 'allied',
     attack: 5,
     defense: 5,
+    damage: 0,
+    sunk: false
   }
 ]
 
@@ -34,37 +38,42 @@ const japanFleet = [
     side: 'japan',
     attack: 3,
     defense: 4,
+    damage: 0,
+    sunk: false
   },
   {
     name: 'Haruna',
     side: 'japan',
     attack: 2,
     defense: 3,
+    damage: 0,
+    sunk: false
   }
 ]
 
 let rolls
 let diceResults = []
 let combatShips = [japanFleet[0], alliedFleet[0]]
-console.log(combatShips);
 
 // Die roller
 function rollDie(rolls) {
   // Clear previous results
-  diceResults.length = 0
+  let results = []
   $('.diceBox').empty()
   // Create any number of dice
   for (let i = 0; i < rolls; i++) {
     //Generate a random number between 1 and 6
     const roll = Math.ceil(Math.random() * 6)
     // Add result to array
-    diceResults.push(roll)
+    results.push(roll)
     // Create a die
     const die = document.createElement('div')
     die.classList.add('die')
     die.innerHTML = roll
     diceBox.appendChild(die)
   }
+  // console.log(results);
+  return results
 }
 
 function buildShip(number, sea, fleet) {
@@ -92,3 +101,27 @@ function buildShip(number, sea, fleet) {
 
 buildShip(1, japanSea, japanFleet)
 buildShip(1, alliedSea, alliedFleet)
+
+
+function attack(ships) {
+  let rolls = ships[0].attack
+  let results = rollDie(rolls)
+  console.log(results);
+  let hits = results.filter(function(result){
+    return result === 6;
+  })
+  rolls = hits.length
+  if (hits.length > 0) {
+    results = rollDie(rolls)
+    results.map(function (result) {
+      ships[1].damage += result
+    })
+  if (ships[1].damage > ships[1].defense) {
+    ships[1].sunk = true
+  }
+  console.log(ships[1]);
+
+
+  }
+
+}
